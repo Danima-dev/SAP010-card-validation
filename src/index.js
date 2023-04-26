5b33f2c64578174946bc0d64cd1d81addaaa4cc1
Daniele Maria <60490226+Danima-dev@users.noreply.github.com>
2023-04-26 09:24:39 -0300

    formulário finalizado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import validator from './validator.js';
 
+document.getElementById("botaoEnviar").addEventListener("click", finalizarCompra)
+
+function finalizarCompra (){alert("hi")}
+
+const numeroDoCartao = document.getElementById("cardNumber").value
+
 console.log(validator);
+
