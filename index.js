1d20b0d4ecca84254493ff2e711fbc855485e406
Daniele Maria <60490226+Danima-dev@users.noreply.github.com>
2023-05-29 10:40:45 -0300

    Updates

---
+++ index.js
@@ -0,0 +1,20 @@
+import validator from './validator.js';
+
+document.getElementById("botaoEnviar").addEventListener("click", finalizarCompra)
+
+function finalizarCompra(e) {
+  e.preventDefault();
+
+  const valorInput = document.getElementById("cardNumber").value
+
+  const a = validator.isValid(valorInput)
+  const cardMask = validator.maskify(valorInput);
+  if (a) {
+    alert('O cartão número:' + cardMask + ' é válido');
+  } else {
+    alert('O cartão número:' + cardMask + ' é inválido');
+  }
+
+
+}
+
---
+++ validator.js
@@ -0,0 +1,42 @@
+const validator = {
+
+  isValid: function (cardNumber) {
+
+    //Inverter a ordem dos números 
+    const inputNumero = cardNumber.split('').reverse().join('')
+    // multiplicar os números  impares
+    let totalDeSoma = 0
+    for (let i = 0; i < inputNumero.length; i++) {
+      let digito = parseInt(inputNumero[i]);
+
+      if (i % 2 === 1) { //Posição par 
+        digito *= 2; //Pegando a posição do digito e multiplicando por 2
+
+        if (digito > 9) {
+          digito -= 9; //Se o número for maior que 9 subtrair 
+        }
+      }
+
+      totalDeSoma += digito;
+
+      //Se o resto da divisão de i por 2 não for 1, mantem o mesmo número
+
+    }
+
+    return totalDeSoma % 10 === 0;
+  },
+  maskify: function (cardNumber) {
+    const x = cardNumber.split('');
+    for (let i = 0; i < x.length - 4; i++) {
+      x[i] = '#';
+    }
+    const maskedCard = x.join('');
+    return maskedCard
+  }
+
+};
+
+
+
+
+export default validator;
